c217f93a29e60ff47b4cfd0590e508d40f32b3a0
kmu-12 <kmu216099@gmail.com>
2026-02-12 10:09:50 -0600

    ummmm did song and worked on playlist
    help wy is this hard

--- U4BLab/src/Playlist.java
+++ U4BLab/src/Playlist.java
@@ -1,2 +1,53 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
 public class Playlist {
+    private ArrayList<Song> songs;
+
+    public Playlist() throws IOException, FileNotFoundException{
+        songs = readText();
+    }
+
+    public ArrayList<Song> readText() throws IOException, FileNotFoundException{
+        Scanner inFile = new Scanner(new File("spotify_unique_years_artists.txt"));
+        ArrayList<Song> list = new ArrayList<Song>();
+        while(inFile.hasNextLine()){
+            String line = inFile.nextLine();
+            String[] split = line.split(",");
+            String t = split[0];
+            String art = split[1];
+            String alb = split[2];
+            String duration = split[3];
+            String year = split[4];
+            int y = Integer.parseInt(year);
+            String g = split[5];
+            Song cancion = new Song(t, art, alb, y, g);
+            list.add(cancion);
+        }
+        return list;
+    }
+
+    public ArrayList<Song> sortArtistAZ(){
+        ArrayList<Song> list = songs;
+        for(int i=0; i<list.size(); i++){
+            for(int j=0; j<list.size(); j++){
+                Song a = list.get(i);
+                Song b = list.get(j);
+                if(a.getArtist().substring(0,1).compareToIgnoreCase(b.getArtist().substring(0,1))<0){
+                        if(i<j){
+                            list.remove(i);
+                            list.add(j-1, a);
+                        }
+                        else{
+                            list.remove(i);
+                            list.add(j, a);
+                        }
+                }
+                if(j==list.size()-1){
+
+                }
+            }
+            i--;
+        }
+    }
 }
--- U4BLab/src/Song.java
+++ U4BLab/src/Song.java
@@ -1,2 +1,22 @@
 public class Song {
+    private String title;
+    private String artist;
+    private String album;
+    private int year;
+    private String genre;
+
+    public Song(String t, String art, String alb, int y, String g){
+        title = t;
+        artist = art;
+        album = alb;
+        year = y;
+        genre = g;
+    }
+
+    public String toString(){
+        String result = String.format("%30s %30s %40s %10d %20s", title, artist, album, year, genre);
+        return result;
+    }
+    public String getArtist(){return artist;}
+
 }
